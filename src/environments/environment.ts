7abaf6bdaa9bb4154127e9fd2810f24bdf75f121
Elvis <elvisloja@gmail.com>
2023-03-11 11:50:15 -0500

    Citasmedicas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyA3OChyEWz3KIuPdnG9H3vRZHR3ZcgZ4ZY",
+  authDomain: "citas-medicas-test-ups-5abf8.firebaseapp.com",
+  projectId: "citas-medicas-test-ups-5abf8",
+  storageBucket: "citas-medicas-test-ups-5abf8.appspot.com",
+  messagingSenderId: "87690533018",
+  appId: "1:87690533018:web:0da42bc5f7ac8f99e22723",
+  measurementId: "G-W001TWJPNB"
+  }
 };
 
 /*
